bda773129c388e4aad8a57b46f85c84a3c379f42
shinkigulati1008 <shinkigulati@gmail.com>
2024-03-27 12:59:47 +1300

    marker not loading -resolved

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,17 @@
 import { useContext } from 'react';
 import { WeatherContext } from '../../contexts/WeatherContext';
+import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+})
+
+L.Marker.prototype.options.icon = DefaultIcon
 const Map = () => {
     const { weatherData } = useContext(WeatherContext);
   return (
